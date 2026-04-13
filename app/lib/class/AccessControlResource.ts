1684ecfe8b0744762fe7680490b70535d443361b
Samu Lang <langsamu@users.noreply.github.com>
2026-04-13 02:23:14 +0100

    Bump @rdfjs/wrapper to 0.32.0 (#63)

--- app/lib/class/AccessControlResource.ts
+++ app/lib/class/AccessControlResource.ts
@@ -1,18 +1,18 @@
-import { NamedNodeAs, NamedNodeFrom, TermAs, TermFrom } from "@rdfjs/wrapper"
+import { NamedNodeAs, NamedNodeFrom, OptionalAs, OptionalFrom, SetFrom, TermAs, TermFrom } from "@rdfjs/wrapper"
 import { AccessControl } from "@/app/lib/class/AccessControl"
 import { ACP } from "@/app/lib/class/Vocabulary"
 import { Typed } from "@/app/lib/class/Typed"
 
 export class AccessControlResource extends Typed {
     get accessControl(): Set<AccessControl> {
-        return this.objects(ACP.accessControl, TermAs.instance(AccessControl), TermFrom.instance)
+        return SetFrom.subjectPredicate(this, ACP.accessControl, TermAs.instance(AccessControl), TermFrom.instance)
     }
 
     get resource(): string | undefined {
-        return this.singularNullable(ACP.resource, NamedNodeAs.string)
+        return OptionalFrom.subjectPredicate(this, ACP.resource, NamedNodeAs.string)
     }
 
     set resource(v: string) {
-        this.overwriteNullable(ACP.resource, v, NamedNodeFrom.string)
+        OptionalAs.object(this, ACP.resource, v, NamedNodeFrom.string)
     }
 }
--- app/lib/class/Matcher.ts
+++ app/lib/class/Matcher.ts
@@ -1,9 +1,9 @@
-import { NamedNodeAs, NamedNodeFrom } from "@rdfjs/wrapper"
+import { NamedNodeAs, NamedNodeFrom, SetFrom } from "@rdfjs/wrapper"
 import { ACP } from "@/app/lib/class/Vocabulary"
 import { Typed } from "@/app/lib/class/Typed"
 
 export class Matcher extends Typed {
     get agent(): Set<string> {
-        return this.objects(ACP.agent, NamedNodeAs.string, NamedNodeFrom.string)
+        return SetFrom.subjectPredicate(this, ACP.agent, NamedNodeAs.string, NamedNodeFrom.string)
     }
 }
