53b7996722238c5d4a9edcc81ecf31d60f89d86b
Precious Oritsedere <rukyjacob@gmail.com>
2026-08-17 16:17:21 +0100

    Remove local RDF mapping classes now provided by @solid/object

--- app/lib/class/Container.ts
+++
@@ -1,9 +0,0 @@
-import { SetFrom, TermAs, TermFrom } from "@rdfjs/wrapper"
-import { Resource } from "@/app/lib/class/Resource"
-import { LDP } from "@/app/lib/class/Vocabulary"
-
-export class Container extends Resource {
-    public get contains(): Set<Resource> {
-        return SetFrom.subjectPredicate(this, LDP.contains, TermAs.instance(Resource), TermFrom.instance)
-    }
-}
